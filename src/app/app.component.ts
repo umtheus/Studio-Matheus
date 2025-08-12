fb7a9aba34d3bc14ce4037426973a50d8cf1d3de
umtheus <gm.arr.matheus@gmail.com>
2025-08-12 00:13:37 -0300

    feat: importa componentes standalone na raiz da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AboutComponent } from 'src/sections/about/about.component';
+import { ContactComponent } from 'src/sections/contact/contact.component';
+import { FooterComponent } from 'src/sections/footer/footer.component';
+import { HomeComponent } from 'src/sections/home/home.component';
+import { ProjectsComponent } from 'src/sections/projects/projects.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
