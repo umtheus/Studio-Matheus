5374fc5868382524e400f56611a798e13f0ad14d
umtheus <gm.arr.matheus@gmail.com>
2025-08-12 00:11:18 -0300

    feat: configura ngx-toastr no AppConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimations(),
+    provideToastr(),
   ],
 };
