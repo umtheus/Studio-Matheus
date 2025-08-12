f67ba27dacc09f75103bcba2ea4467d6251e692a
umtheus <gm.arr.matheus@gmail.com>
2025-08-12 02:20:25 -0300

    test(app): remove teste de welcome e adicionar componentes de seção ao teste do AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
+import { HomeComponent } from 'src/sections/home/home.component';
+import { AboutComponent } from 'src/sections/about/about.component';
+import { ProjectsComponent } from 'src/sections/projects/projects.component';
+import { ContactComponent } from 'src/sections/contact/contact.component';
+import { FooterComponent } from 'src/sections/footer/footer.component';
+import { SkillsComponent } from 'src/sections/home/skills/skills.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        RouterModule.forRoot([]),
+        HomeComponent,
+        AboutComponent,
+        ProjectsComponent,
+        ContactComponent,
+        FooterComponent,
+        SkillsComponent,
+      ],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome portfolio'
-    );
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'portfolio'`, () => {
