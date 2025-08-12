2e36473f328a49aa46f9ab65d87d8940489becb2
umtheus <gm.arr.matheus@gmail.com>
2025-08-12 02:21:41 -0300

    test(contact): adiciona testes de envio e proteção contra bots e testa envio por emails, whtsApp

--- src/sections/contact/contact.component.spec.ts
+++ src/sections/contact/contact.component.spec.ts
@@ -18,4 +18,37 @@ describe('ContactComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('deve bloquear envio se honeypot estiver preenchido', () => {
+    component.honeypot = 'sou um bot';
+    const spy = jest.spyOn(window, 'open');
+    component.handleSubmit();
+    expect(spy).not.toHaveBeenCalled();
+  });
+  it('deve abrir link de email se opção for "email"', () => {
+    component.selectedOption = 'email';
+    component.message = 'Olá!';
+    component.honeypot = ''; // campo limpo
+
+    const spy = jest.spyOn(window, 'open').mockImplementation(() => null);
+    component.handleSubmit();
+
+    expect(spy).toHaveBeenCalledWith(
+      expect.stringContaining('mailto:gm.arr.matheus@gmail.com'),
+      '_blank'
+    );
+  });
+
+  it('deve abrir link do WhatsApp se opção for "whatsapp"', () => {
+    component.selectedOption = 'whatsapp';
+    component.message = 'Olá!';
+    component.honeypot = ''; // campo limpo
+
+    const spy = jest.spyOn(window, 'open').mockImplementation(() => null);
+    component.handleSubmit();
+
+    expect(spy).toHaveBeenCalledWith(
+      expect.stringContaining('https://wa.me/5518996407118'),
+      '_blank'
+    );
+  });
 });
